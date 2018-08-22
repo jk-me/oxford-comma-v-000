062385c1c2f4850d55549adf1fe354f6e5b747b5
Jenny K <jkam17@gmail.com>
2018-08-22 00:10:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-  last=array.pop
-  str=array.join(', ')
-  str << ", and #{last}"
+  if array.length==1
+    return array.join
+  elsif array.length==2
+    return array.join(' and ')
+  else 
+    last=array.pop
+    str=array.join(', ')
+    str << ", and #{last}"
+    
 end
